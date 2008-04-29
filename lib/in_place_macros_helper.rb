c7b067a13f3fbd92bdf648ca55b54edb7dd3b03f
Christopher Redinger <redinger@gmail.com>
2008-04-29 16:16:44 -0400

    Added default value for in place editor field text

--- lib/in_place_macros_helper.rb
+++ lib/in_place_macros_helper.rb
@@ -75,9 +75,14 @@ module InPlaceMacrosHelper
   # Renders the value of the specified object and method with in-place editing capabilities.
   def in_place_editor_field(object, method, tag_options = {}, in_place_editor_options = {})
     tag = ::ActionView::Helpers::InstanceTag.new(object, method, self)
+    value = tag.value(tag.object)
+    if (value.blank?)
+      value = tag_options.delete(:default_value)
+    end
+    
     tag_options = {:tag => "span", :id => "#{object}_#{method}_#{tag.object.id}_in_place_editor", :class => "in_place_editor_field"}.merge!(tag_options)
     in_place_editor_options[:url] = in_place_editor_options[:url] || url_for({ :action => "set_#{object}_#{method}", :id => tag.object.id })
-    tag.to_content_tag(tag_options.delete(:tag), tag_options) +
+    tag.content_tag(tag_options.delete(:tag), value, tag_options) +    
     in_place_editor(tag_options[:id], in_place_editor_options)
   end
 end
